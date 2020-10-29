08736f4cb4407f4761d8c890eac380a69ba3f862
aman tiwari <amantiwari.kanpur20@gmail.com>
2020-10-29 14:40:13 +0530

    Updates

---
+++ precache-manifest.493056ac65a7e8e781c1243f4d2515c6.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a178232d4cd6b3b59881e0384959d29",
+    "url": "/Gold_gym/index.html"
+  },
+  {
+    "revision": "e84b90a5e3c4903982a2",
+    "url": "/Gold_gym/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "1279960bf9d30fc6a45d",
+    "url": "/Gold_gym/static/css/main.f5ffe421.chunk.css"
+  },
+  {
+    "revision": "e84b90a5e3c4903982a2",
+    "url": "/Gold_gym/static/js/2.3d5b7532.chunk.js"
+  },
+  {
+    "revision": "6f7978863a096bf76e594b084fd7dfd8",
+    "url": "/Gold_gym/static/js/2.3d5b7532.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1279960bf9d30fc6a45d",
+    "url": "/Gold_gym/static/js/main.eb87e4d2.chunk.js"
+  },
+  {
+    "revision": "4133674fdc86eeb07c97",
+    "url": "/Gold_gym/static/js/runtime-main.b7c9af5e.js"
+  },
+  {
+    "revision": "89f263ffde0a7acecd2418e02eacdfc6",
+    "url": "/Gold_gym/static/media/app2.89f263ff.jpg"
+  },
+  {
+    "revision": "f3e78fc1842b2d3d2a11f9cb6c20f0cd",
+    "url": "/Gold_gym/static/media/bk.f3e78fc1.jpg"
+  },
+  {
+    "revision": "c037c8a76e7d664d8282aa3b585c1bdb",
+    "url": "/Gold_gym/static/media/blog2.c037c8a7.webp"
+  },
+  {
+    "revision": "9c233c5da1f90e84d34d55d93f9f4ce1",
+    "url": "/Gold_gym/static/media/con.9c233c5d.jpg"
+  },
+  {
+    "revision": "2ded7560b9f122b05f1bc2d3658a6653",
+    "url": "/Gold_gym/static/media/fit.2ded7560.jpg"
+  },
+  {
+    "revision": "98b190feede241b1a9054704d1fc6688",
+    "url": "/Gold_gym/static/media/fit2.98b190fe.jpg"
+  },
+  {
+    "revision": "32a05cfc6ec531863e6a52fd2e094710",
+    "url": "/Gold_gym/static/media/footer.32a05cfc.jpg"
+  },
+  {
+    "revision": "b85b1b2198772f0b475c0d7bd14107c0",
+    "url": "/Gold_gym/static/media/gym.b85b1b21.jpg"
+  },
+  {
+    "revision": "e70ec317fe7846038946e2d32ca11107",
+    "url": "/Gold_gym/static/media/gym4.e70ec317.jpg"
+  },
+  {
+    "revision": "6c067c92b96746feba4869137ab65f5a",
+    "url": "/Gold_gym/static/media/gym5.6c067c92.jpg"
+  },
+  {
+    "revision": "608886e5eb1cc50f2930a47b748d3509",
+    "url": "/Gold_gym/static/media/home1.608886e5.jpg"
+  },
+  {
+    "revision": "b304566b06e90b7503ef52b7d58009c7",
+    "url": "/Gold_gym/static/media/home2.b304566b.jpg"
+  },
+  {
+    "revision": "27745883915363e7397a095ca3d4fc3c",
+    "url": "/Gold_gym/static/media/home3.27745883.jpg"
+  },
+  {
+    "revision": "7572ae46e317f27e49efdafad1f9f14a",
+    "url": "/Gold_gym/static/media/home5.7572ae46.jpg"
+  },
+  {
+    "revision": "9e5f197b209b66200a357035452ad9d9",
+    "url": "/Gold_gym/static/media/home6.9e5f197b.jpg"
+  },
+  {
+    "revision": "fd77d7fdbbcda7068ad0664b0255c687",
+    "url": "/Gold_gym/static/media/home7.fd77d7fd.jpg"
+  },
+  {
+    "revision": "36b7b28305cb9f885c3b175bc9934357",
+    "url": "/Gold_gym/static/media/img10.36b7b283.jpg"
+  },
+  {
+    "revision": "3a0eec63b1749cf97a12636a4a0120c7",
+    "url": "/Gold_gym/static/media/img11.3a0eec63.jpg"
+  },
+  {
+    "revision": "db49350ba2b6d67cc542b73e20afce77",
+    "url": "/Gold_gym/static/media/img12.db49350b.jpg"
+  },
+  {
+    "revision": "2041b27c016a0a5edba595e62cd20a44",
+    "url": "/Gold_gym/static/media/img13.2041b27c.jpg"
+  },
+  {
+    "revision": "44643e0827520229520db704bff1e972",
+    "url": "/Gold_gym/static/media/img14.44643e08.jpg"
+  },
+  {
+    "revision": "5d2d34f3d293bd4a137a5189fb9abb8d",
+    "url": "/Gold_gym/static/media/img2.5d2d34f3.png"
+  },
+  {
+    "revision": "95b9d30ee035d422f0c419a16e08d2a1",
+    "url": "/Gold_gym/static/media/img2.95b9d30e.jpg"
+  },
+  {
+    "revision": "8f8a47c2b47d4f9d8a8d1fe6b50551fe",
+    "url": "/Gold_gym/static/media/img3.8f8a47c2.png"
+  },
+  {
+    "revision": "ce686b2cfb3798e48fd276f1513b03b6",
+    "url": "/Gold_gym/static/media/img3.ce686b2c.jpeg"
+  },
+  {
+    "revision": "7aeba3a2d3f912f7c3bd87151e9a51a2",
+    "url": "/Gold_gym/static/media/img4.7aeba3a2.png"
+  },
+  {
+    "revision": "882f5317d48fa12ee5108be79cbb96d2",
+    "url": "/Gold_gym/static/media/img5.882f5317.webp"
+  },
+  {
+    "revision": "94a9889f738774cf09e125eb70f6b2d7",
+    "url": "/Gold_gym/static/media/img6.94a9889f.jpg"
+  },
+  {
+    "revision": "fc73a33566e784befd3f0963cd1f68e3",
+    "url": "/Gold_gym/static/media/img9.fc73a335.jpg"
+  },
+  {
+    "revision": "61cc927d0986a183ac14217eafeb4481",
+    "url": "/Gold_gym/static/media/logo3.61cc927d.png"
+  },
+  {
+    "revision": "0e52891e94b7b30441e4029dc81d96e6",
+    "url": "/Gold_gym/static/media/logo4.0e52891e.png"
+  },
+  {
+    "revision": "3a8ac6eb62c76dd246ecd7ea61cfd06c",
+    "url": "/Gold_gym/static/media/logo5.3a8ac6eb.png"
+  },
+  {
+    "revision": "99f89c59a8b9ea006623bc7fa2d00f9e",
+    "url": "/Gold_gym/static/media/n1.99f89c59.jpg"
+  },
+  {
+    "revision": "e2f2bed64924a5325ba10aac83f1ace8",
+    "url": "/Gold_gym/static/media/signup.e2f2bed6.jpg"
+  },
+  {
+    "revision": "6e38a01e413c8471f19630ecb85ed9c1",
+    "url": "/Gold_gym/static/media/tr.6e38a01e.jpg"
+  },
+  {
+    "revision": "3371e12b5e69b17317b1d9d9d6f40bed",
+    "url": "/Gold_gym/static/media/tr2.3371e12b.jpg"
+  },
+  {
+    "revision": "0aef3e32fd37341457f3245d5a8aefed",
+    "url": "/Gold_gym/static/media/tr3.0aef3e32.webp"
+  },
+  {
+    "revision": "3171c18f3844d7fad51cdb6ebfcda642",
+    "url": "/Gold_gym/static/media/tr5.3171c18f.webp"
+  },
+  {
+    "revision": "d1f2c7bf008446835f76e6dd9d7da609",
+    "url": "/Gold_gym/static/media/tr6.d1f2c7bf.jpeg"
+  }
+]);
